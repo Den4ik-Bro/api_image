b42ba1f8ae68cc6427dcdce78e0836061ac423d5
Den4ik-Bro <denis.rr@bk.ru>
2022-03-28 00:15:59 +0300

    work

--- image/serializers.py
+++ image/serializers.py
@@ -8,13 +8,6 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('picture', 'name', 'url', 'parent_picture')
 
-    def validate(self, data):
-        if 'width' not in data and 'height' not in data:
-            raise serializers.ValidationError('no date given')
-        if 'url' in data and 'file' in data:
-            raise serializers.ValidationError('not an explicit file path')
-        return data
-
 
 class UploadImageSerializer(serializers.Serializer):
     url = serializers.URLField(required=False)
